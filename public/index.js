fbd838730161d127ad8da2f6a6dfaa519b6ac591
Steven Inouye <steveninouye@msn.com>
2019-01-10 09:47:15 -0800

    Start helper classes

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
-alert('hello world');
+function Screen(width, height) {
+
+}
+
+Screen.prototype.drawSprite = function(sp,x,y) {
+
+}
+
+function Sprite(img, x,y,w,h) {
+
+}
+
+function InputHandler(){
+
+}
+
+InputHandler.prototype.isDown = function(code) {
+
+}
+
+InputHandler.prototype.isPressed = function(code) {
+  
+}
