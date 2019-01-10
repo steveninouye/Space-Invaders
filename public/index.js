144eed43a05bf6811e26aea86bc4ea21b3bfdda8
Steven Inouye <steveninouye@msn.com>
2019-01-10 09:54:55 -0800

    Add class to handle canvas and to listen for key events

--- public/index.js
+++ public/index.js
@@ -1,23 +1,37 @@
 function Screen(width, height) {
+  this.canvas = document.createElement('canvas');
+  this.canvas.width = this.width = width;
+  this.canvas.height = this.height = height;
+  this.ctx = this.canvas.getContext('2d');
 
+  document.body.appendChild(this.canvas);
 }
 
-Screen.prototype.drawSprite = function(sp,x,y) {
+Screen.prototype.drawSprite = function(sp, x, y) {
+  this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
+};
 
+function Sprite(img, x, y, w, h) {
+  this.img = img;
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
 }
 
-function Sprite(img, x,y,w,h) {
-
+function InputHandler() {
+  this.down = {};
+  this.pressed = {};
+  let _this = this;
+  document.addEventListener('keydown', function(evt) {
+    _this.down[evt.keyCode] = true;
+  });
+  document.addEventListener('keyup', function(evt) {
+    delete _this.down[evt.keyCode];
+    delete _this.pressed[evt.keyCode];
+  });
 }
 
-function InputHandler(){
+InputHandler.prototype.isDown = function(code) {};
 
-}
-
-InputHandler.prototype.isDown = function(code) {
-
-}
-
-InputHandler.prototype.isPressed = function(code) {
-  
-}
+InputHandler.prototype.isPressed = function(code) {};
